8166d7892ca80b4f12447e218bd1632770689383
csadrian <cs.adrian@gmail.com>
2020-02-14 15:12:36 +0100

    Updated dog

--- train.py
+++ train.py
@@ -1,14 +1,20 @@
 from tensorflow.keras.applications.resnet import ResNet50
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import Model
+import tensorflow.keras.models as models
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras import layers
+
 from tensorflow.keras import backend as K
 import tensorflow as tf
 import numpy as np
 import sys
+import os
 
+import neptune
 
 import gin
+import gin.config
 import gin.tf
 
 
@@ -22,10 +28,12 @@ flags.DEFINE_multi_string(
 FLAGS = flags.FLAGS
 
 
+
+
 from tensorflow.python.keras.backend import set_session
 
 
-batch_size = 16
+batch_size = 6
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
@@ -56,7 +64,7 @@ test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(batch_size)
 loss_object = tf.keras.losses.CategoricalCrossentropy(reduction=tf.keras.losses.Reduction.NONE)
 nored_loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False, reduction=tf.keras.losses.Reduction.NONE)
 
-optimizer = tf.keras.optimizers.SGD(learning_rate=0.001)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 
 train_loss = tf.keras.metrics.Mean(name='train_loss')
 train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
@@ -82,8 +90,18 @@ class MyModel(Model):
     x = self.dense_out(x)
     return x
 
-model = MyModel()
-model.build((batch_size, 64, 64, 3))
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(10))
+#model.add(layers.Activation('softmax'))
+#model = MyModel()
+
+model.build((batch_size, 32, 32, 3))
 
 
 @tf.function
@@ -98,41 +116,33 @@ def proj(u, v):
   return (tf.matmul(tf.transpose(u), v) / tf.matmul(tf.transpose(u), u)) * u
 
 @tf.function
-def train_step(images, labels, c_grads):
-  print("train step start")
+@gin.configurable(blacklist=['images', 'labels'])
+def train_step(images, labels, c_grads, dog_lambda=0.0):
   with tf.GradientTape() as tape2:
    with tf.GradientTape() as tape:
     predictions = model(images, training=True)
     #nored_loss = ce(tf.one_hot(labels, 10), tf.nn.softmax(predictions))
     #loss = tf.reduce_sum(nored_loss)
     loss = loss_object(tf.one_hot(labels, 10), tf.nn.softmax(predictions))
-    #loss = tf.reshape(lBoss, (-1, 1))
-    #loss = tf.losses.categorical_crossentropy(tf.one_hot(labels, 10), tf.nn.softmax(predictions))
    gradients = tape.jacobian(loss, model.trainable_variables, experimental_use_pfor=False)
    reg_loss = 0.0
    for grad in gradients:
+     if len(grad.get_shape()) != 5:
+       continue
      grad = tf.reshape(grad, (batch_size, -1))
-     #M = tf.matmul(tf.transpose(grad), grad)
      M = tf.matmul(grad, tf.transpose(grad))
-     reg_loss += tf.math.abs(M)
-     #reg_loss += tf.math.abs(M-tf.eye(M.get_shape()[0]))
-
-     #reg_loss += tf.math.abs(M * (tf.ones((M.get_shape()[0], M.get_shape()[0]))-tf.eye(M.get_shape()[0])))
-   reg_loss = reg_loss / batch_size
-  #gradients_reg = tape2.jacobian(reg_loss, model.trainable_variables, experimental_use_pfor=False)
+     #reg_loss += tf.reduce_sum(tf.math.abs(grad), axis=[0,1])
+     reg_loss += tf.reduce_sum(tf.math.abs(M-tf.eye(M.get_shape()[0])))
+     #reg_loss += tf.reduce_mean(tf.math.abs(M * (tf.ones((M.get_shape()[0], M.get_shape()[0]))-tf.eye(M.get_shape()[0]))))
+   reg_loss = reg_loss / (batch_size)
   gradients_reg = tape2.gradient(reg_loss, model.trainable_variables)
-  g = []
-  #print(len(gradients_reg))
-  #print(len(gradients))
+
   
+  g = []
   for x, y in zip(gradients, gradients_reg):
-      #y = tf.reduce_mean(y)
       x = tf.reduce_mean(x, axis=0)
-      #x = tf.reduce_mean(x)
-
-      print(x, y)
-      g.append(x+0.00000001*y)
-
+      g.append(x+dog_lambda*y)
+  
   if False:
     i = 0
     updated_gradients = []
@@ -148,7 +158,7 @@ def train_step(images, labels, c_grads):
       i += 1
 
     gradients = updated_gradients
-      
+
   optimizer.apply_gradients(zip(g, model.trainable_variables))
 
   train_loss(tf.reduce_mean(reg_loss))
@@ -173,7 +183,7 @@ def ssr_step(images, labels, ssr_alpha=0.01):
       updated_gradients.append(ssr_alpha*r_o)
     gradients = updated_gradients
     
-  optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+  optimizer.apply_gradients(zip(gradients_both, model.trainable_variables))
 
   train_loss(loss)
   train_accuracy(labels, predictions)
@@ -210,9 +220,9 @@ def trainer(num_epochs=10, ssr_steps=1):
       for i in range(ssr_steps):
         ssr_step(images, labels)
       step += 1
-      if step % 100 == 0:
-        for var in model.trainable_variables:
-          c_grads[var.experimental_ref()].assign(tf.ones(tf.reshape(var, (-1, 1)).get_shape()))
+      #if step % 100 == 0:
+      #  for var in model.trainable_variables:
+      #    c_grads[var.experimental_ref()].assign(tf.ones(tf.reshape(var, (-1, 1)).get_shape()))
 
     for test_images, test_labels in test_ds:
       test_step(test_images, test_labels)
@@ -226,6 +236,15 @@ def trainer(num_epochs=10, ssr_steps=1):
 
 def main(argv):
   gin.parse_config_files_and_bindings(FLAGS.gin_file, FLAGS.gin_param)
+
+  use_neptune = "NEPTUNE_API_TOKEN" in os.environ
+  if False and use_neptune:
+    neptune.init(project_qualified_name="csadrian/dog")
+    print(gin.config._CONFIG)
+    exp = neptune.create_experiment(params=gin.config._CONFIG, name="exp")
+    #for tag in opts['tags'].split(','):
+    #  neptune.append_tag(tag)
+
   trainer()
 
 if __name__ == '__main__':
