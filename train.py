edac671a7721a6d3db80edb742771a8dcb60f917
csadrian <cs.adrian@gmail.com>
2020-02-17 17:35:57 +0100

    Added resnet and lr schedule

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from tensorflow.keras.applications.resnet import ResNet50
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import Model
 import tensorflow.keras.models as models
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Input, AveragePooling2D, Conv2D, BatchNormalization, Activation, Flatten
 from tensorflow.keras import layers
 from tensorflow.keras import regularizers
 
@@ -29,14 +29,14 @@ flags.DEFINE_multi_string(
 FLAGS = flags.FLAGS
 
 
-batch_size = 6
+batch_size = 20
 
 HEIGHT = 32
 WIDTH = 32
 NUM_CHANNELS = 3
 NUM_TRAIN_SAMPLES = 50000
 BS_PER_GPU = batch_size
- 
+
 
 from tensorflow.python.keras.backend import set_session
 
@@ -55,18 +55,30 @@ def augmentation(x, y):
     return x, y	
 
 
-#@tf.function
-def schedule(epoch):
-  initial_learning_rate = BASE_LEARNING_RATE * BS_PER_GPU / 128
-  learning_rate = initial_learning_rate
-  for mult, start_epoch in LR_SCHEDULE:
-    if epoch >= start_epoch:
-      learning_rate = initial_learning_rate * mult
-    else:
-      break
-  tf.summary.scalar('learning rate', data=learning_rate, step=epoch)
-  return learning_rate
 
+def lr_schedule(epoch):
+    """Learning Rate Schedule
+
+    Learning rate is scheduled to be reduced after 80, 120, 160, 180 epochs.
+    Called automatically every epoch as part of callbacks during training.
+
+    # Arguments
+        epoch (int): The number of epochs
+
+    # Returns
+        lr (float32): learning rate
+    """
+    lr = 1e-3
+    if epoch > 90:
+        lr *= 0.5e-3
+    elif epoch > 80:
+        lr *= 1e-3
+    elif epoch > 60:
+        lr *= 1e-2
+    elif epoch > 40:
+        lr *= 1e-1
+    print('Learning rate: ', lr)
+    return lr
 
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
@@ -91,11 +103,20 @@ iters_per_epoch = train_size // batch_size
 
 
 
+step = tf.Variable(0, trainable=False)
+boundaries = [100000, 200000]
+values = [0.001, 0.0001, 0.00001]
+learning_rate_fn = tf.keras.optimizers.schedules.PiecewiseConstantDecay(
+    boundaries, values)
+
+# Later, whenever we perform an optimization step, we pass in the step.
+learning_rate = learning_rate_fn(step)
+
 
 loss_object = tf.keras.losses.CategoricalCrossentropy(reduction=tf.keras.losses.Reduction.NONE)
 nored_loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False, reduction=tf.keras.losses.Reduction.NONE)
 
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
 
 train_sup_loss = tf.keras.metrics.Mean(name='train_sup_loss')
 train_reg_loss = tf.keras.metrics.Mean(name='train_reg_loss')
@@ -105,76 +126,235 @@ test_sup_loss = tf.keras.metrics.Mean(name='test_sup_loss')
 test_reg_loss = tf.keras.metrics.Mean(name='test_reg_loss')
 test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
 
-class MyModel(Model):
-  def __init__(self):
-    super(MyModel, self).__init__()
-    self.base_model = ResNet50(include_top=False)
 
-    self.dense_1 = Dense(1024, activation='relu')
-    self.dense_out = Dense(10, activation='softmax')
 
-    #self._trainable_weights.extend(self.dense_1.trainable_weights)
-    #self._trainable_weights.extend(self.dense_out.trainable_weights)
+def resnet_layer(inputs,
+                 num_filters=16,
+                 kernel_size=3,
+                 strides=1,
+                 activation='relu',
+                 batch_normalization=True,
+                 conv_first=True):
+    """2D Convolution-Batch Normalization-Activation stack builder
 
-  def call(self, x, training=True):
-    x = self.base_model(x)
-    x = GlobalAveragePooling2D()(x)
-    x = self.dense_1(x)
-    x = self.dense_out(x)
+    # Arguments
+        inputs (tensor): input tensor from input image or previous layer
+        num_filters (int): Conv2D number of filters
+        kernel_size (int): Conv2D square kernel dimensions
+        strides (int): Conv2D square stride dimensions
+        activation (string): activation name
+        batch_normalization (bool): whether to include batch normalization
+        conv_first (bool): conv-bn-activation (True) or
+            bn-activation-conv (False)
+
+    # Returns
+        x (tensor): tensor as input to the next layer
+    """
+    conv = Conv2D(num_filters,
+                  kernel_size=kernel_size,
+                  strides=strides,
+                  padding='same',
+                  kernel_initializer='he_normal',
+                  kernel_regularizer=regularizers.l2(1e-4))
+
+    x = inputs
+    if conv_first:
+        x = conv(x)
+        if batch_normalization:
+            x = BatchNormalization(fused=False)(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+    else:
+        if batch_normalization:
+            x = BatchNormalization(fused=False)(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+        x = conv(x)
     return x
 
 
-conv_tensors = []
+def resnet_v1(input_shape, depth, num_classes=10):
+    """ResNet Version 1 Model builder [a]
+
+    Stacks of 2 x (3 x 3) Conv2D-BN-ReLU
+    Last ReLU is after the shortcut connection.
+    At the beginning of each stage, the feature map size is halved (downsampled)
+    by a convolutional layer with strides=2, while the number of filters is
+    doubled. Within each stage, the layers have the same number filters and the
+    same number of filters.
+    Features maps sizes:
+    stage 0: 32x32, 16
+    stage 1: 16x16, 32
+    stage 2:  8x8,  64
+    The Number of parameters is approx the same as Table 6 of [a]:
+    ResNet20 0.27M
+    ResNet32 0.46M
+    ResNet44 0.66M
+    ResNet56 0.85M
+    ResNet110 1.7M
+
+    # Arguments
+        input_shape (tensor): shape of input image tensor
+        depth (int): number of core convolutional layers
+        num_classes (int): number of classes (CIFAR10 has 10)
+
+    # Returns
+        model (Model): Keras model instance
+    """
+    if (depth - 2) % 6 != 0:
+        raise ValueError('depth should be 6n+2 (eg 20, 32, 44 in [a])')
+    # Start model definition.
+    num_filters = 16
+    num_res_blocks = int((depth - 2) / 6)
+
+    inputs = Input(shape=input_shape)
+    x = resnet_layer(inputs=inputs)
+    # Instantiate the stack of residual units
+    for stack in range(3):
+        for res_block in range(num_res_blocks):
+            strides = 1
+            if stack > 0 and res_block == 0:  # first layer but not first stack
+                strides = 2  # downsample
+            y = resnet_layer(inputs=x,
+                             num_filters=num_filters,
+                             strides=strides)
+            y = resnet_layer(inputs=y,
+                             num_filters=num_filters,
+                             activation=None)
+            if stack > 0 and res_block == 0:  # first layer but not first stack
+                # linear projection residual shortcut connection to match
+                # changed dims
+                x = resnet_layer(inputs=x,
+                                 num_filters=num_filters,
+                                 kernel_size=1,
+                                 strides=strides,
+                                 activation=None,
+                                 batch_normalization=False)
+            x = tf.keras.layers.add([x, y])
+            x = Activation('relu')(x)
+        num_filters *= 2
+
+    # Add classifier on top.
+    # v1 does not use BN after last shortcut connection-ReLU
+    x = AveragePooling2D(pool_size=8)(x)
+    y = Flatten()(x)
+    outputs = Dense(num_classes,
+                    #activation='softmax',
+                    kernel_initializer='he_normal')(y)
+
+    # Instantiate model.
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+def resnet_v2(input_shape, depth, num_classes=10):
+    """ResNet Version 2 Model builder [b]
+
+    Stacks of (1 x 1)-(3 x 3)-(1 x 1) BN-ReLU-Conv2D or also known as
+    bottleneck layer
+    First shortcut connection per layer is 1 x 1 Conv2D.
+    Second and onwards shortcut connection is identity.
+    At the beginning of each stage, the feature map size is halved (downsampled)
+    by a convolutional layer with strides=2, while the number of filter maps is
+    doubled. Within each stage, the layers have the same number filters and the
+    same filter map sizes.
+    Features maps sizes:
+    conv1  : 32x32,  16
+    stage 0: 32x32,  64
+    stage 1: 16x16, 128
+    stage 2:  8x8,  256
+
+    # Arguments
+        input_shape (tensor): shape of input image tensor
+        depth (int): number of core convolutional layers
+        num_classes (int): number of classes (CIFAR10 has 10)
+
+    # Returns
+        model (Model): Keras model instance
+    """
+    if (depth - 2) % 9 != 0:
+        raise ValueError('depth should be 9n+2 (eg 56 or 110 in [b])')
+    # Start model definition.
+    num_filters_in = 16
+    num_res_blocks = int((depth - 2) / 9)
+
+    inputs = Input(shape=input_shape)
+    # v2 performs Conv2D with BN-ReLU on input before splitting into 2 paths
+    x = resnet_layer(inputs=inputs,
+                     num_filters=num_filters_in,
+                     conv_first=True)
+
+    # Instantiate the stack of residual units
+    for stage in range(3):
+        for res_block in range(num_res_blocks):
+            activation = 'relu'
+            batch_normalization = True
+            strides = 1
+            if stage == 0:
+                num_filters_out = num_filters_in * 4
+                if res_block == 0:  # first layer and first stage
+                    activation = None
+                    batch_normalization = False
+            else:
+                num_filters_out = num_filters_in * 2
+                if res_block == 0:  # first layer but not first stage
+                    strides = 2    # downsample
+
+            # bottleneck residual unit
+            y = resnet_layer(inputs=x,
+                             num_filters=num_filters_in,
+                             kernel_size=1,
+                             strides=strides,
+                             activation=activation,
+                             batch_normalization=batch_normalization,
+                             conv_first=False)
+            y = resnet_layer(inputs=y,
+                             num_filters=num_filters_in,
+                             conv_first=False)
+            y = resnet_layer(inputs=y,
+                             num_filters=num_filters_out,
+                             kernel_size=1,
+                             conv_first=False)
+            if res_block == 0:
+                # linear projection residual shortcut connection to match
+                # changed dims
+                x = resnet_layer(inputs=x,
+                                 num_filters=num_filters_out,
+                                 kernel_size=1,
+                                 strides=strides,
+                                 activation=None,
+                                 batch_normalization=False)
+            x = tf.keras.layers.add([x, y])
+
+        num_filters_in = num_filters_out
+
+    # Add classifier on top.
+    # v2 has BN-ReLU before Pooling
+    x = BatchNormalization(fused=False)(x)
+    x = Activation('relu')(x)
+    x = AveragePooling2D(pool_size=8)(x)
+    y = Flatten()(x)
+    outputs = Dense(num_classes,
+                    #activation='softmax',
+                    kernel_initializer='he_normal')(y)
+
+    # Instantiate model.
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
 
 @gin.configurable()
-def build_model(weight_decay=1e-5):
+def build_model(depth=14, weight_decay=1e-5, version=1):
 
-  model = models.Sequential()
+    input_shape=(32, 32, 3)
 
-  model.add(layers.Conv2D(32, (3, 3), kernel_regularizer=regularizers.l2(weight_decay), input_shape=(32, 32, 3)))
-  #model.add(layers.BatchNormalization())
-  model.add(layers.LayerNormalization())
-  model.add(layers.Activation('relu'))
+    if version == 2:
+        model = resnet_v2(input_shape=input_shape, depth=depth)
+    else:
+        model = resnet_v1(input_shape=input_shape, depth=depth)
 
-  model.add(layers.Conv2D(32, (3, 3), kernel_regularizer=regularizers.l2(weight_decay)))
-  #model.add(layers.BatchNormalization())
-  model.add(layers.LayerNormalization())
-  model.add(layers.Activation('relu'))
-
-  model.add(layers.MaxPooling2D((2, 2)))
-
-  model.add(layers.Conv2D(64, (3, 3), kernel_regularizer=regularizers.l2(weight_decay)))
-  #model.add(layers.BatchNormalization())
-  model.add(layers.LayerNormalization())
-  model.add(layers.Activation('relu'))
-
-  model.add(layers.Conv2D(64, (3, 3), kernel_regularizer=regularizers.l2(weight_decay)))
-  #model.add(layers.BatchNormalization())
-  model.add(layers.LayerNormalization())
-  model.add(layers.Activation('relu'))
-
-  model.add(layers.MaxPooling2D((2, 2)))
-
-  model.add(layers.Conv2D(128, (3, 3), kernel_regularizer=regularizers.l2(weight_decay)))
-  #model.add(layers.BatchNormalization())
-  model.add(layers.LayerNormalization())
-  model.add(layers.Activation('relu'))
-
-  model.add(layers.Flatten())
-  model.add(layers.Dense(10))
-  model.build((batch_size, 32, 32, 3))
-
-  for layer in model.layers:
-    if isinstance(layer, layers.Conv2D):
-      conv_tensors.extend(layer.variables)
-      print(type(layer))
-
-  return model
+    return model
 
 model = build_model()
-
-#model.add(layers.Activation('softmax'))
-#model = MyModel()
 
 
 
